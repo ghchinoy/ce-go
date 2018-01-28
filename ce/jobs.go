6397dbcbc7986d599f6d82e59c54e46c87dfd353
G. Hussain Chinoy <ghchinoy@gmail.com>
2018-01-28 13:51:58 -0700

    Adds list jobs api implementation

--- ce/jobs.go
+++ ce/jobs.go
@@ -1,5 +1,13 @@
 package ce
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/moul/http2curl"
+)
+
 // Job represents an scheduled job on the platform
 type Job struct {
 	ID                 string     `json:"id"`
@@ -29,4 +37,26 @@ type JobTrigger struct {
 	EndTime      int    `json:"endTime"`
 	Priority     int    `json:"priority"`
 	State        string `json:"state"`
+}
+
+// ListJobs lists jobs on the Platform
+func ListJobs(base, auth string) ([]byte, int, string, error) {
+	var bodybytes []byte
+	url := fmt.Sprintf("%s%s", base, "/jobs")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return bodybytes, -1, "", err
+	}
+	req.Header.Add("Authorization", auth)
+	req.Header.Add("Accept", "application/json")
+	curlCmd, _ := http2curl.GetCurlCommand(req)
+	curl := fmt.Sprintf("%s", curlCmd)
+	resp, err := client.Do(req)
+	if err != nil {
+		return bodybytes, resp.StatusCode, curl, err
+	}
+	bodybytes, err = ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	return bodybytes, resp.StatusCode, curl, nil
 }
