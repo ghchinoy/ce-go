37cb0a3e5c8358a5301d197b714c5bf35fc37dfa
G. Hussain Chinoy <ghchinoy@gmail.com>
2018-03-20 22:32:06 -0700

    Adding default branding object

--- ce/branding.go
+++ ce/branding.go
@@ -16,29 +16,92 @@ const (
 )
 
 // BrandingConfig represents the configuration of a Platform's branding
+// TODO generate this struct via a GET to /organizations/branding
+// as the configuration values change frequently; possibly during test suite
 type BrandingConfig struct {
-	HeaderFont                     string `json:"headerFont,omitempty"`
-	HeaderColor                    string `json:"headerColor,omitempty"`
-	BodyFont                       string `json:"bodyFont,omitempty"`
-	BodyColor                      string `json:"bodyColor,omitempty"`
-	Logo                           string `json:"logo,omitempty"`
-	Favicon                        string `json:"favicon,omitempty"`
-	ThemePrimaryColor              string `json:"themePrimaryColor,omitempty"`
-	ThemeHighlightColor            string `json:"themeHighlightColor,omitempty"`
-	ButtonPrimaryBackgroundColor   string `json:"buttonPrimaryBackgroundColor,omitempty"`
-	ButtonPrimaryTextColor         string `json:"buttonPrimaryTextColor,omitempty"`
-	ButtonSecondaryBackgroundColor string `json:"buttonSecondaryBackgroundColor,omitempty"`
-	ButtonSecondaryTextColor       string `json:"buttonSecondaryTextColor,omitempty"`
-	ButtonDeleteBackgroundColor    string `json:"buttonDeleteBackgroundColor,omitempty"`
-	ButtonDeleteTextColor          string `json:"buttonDeleteTextColor,omitempty"`
-	LogoBackgroundColor            string `json:"logoBackgroundColor,omitempty"`
-	TopBarBackgroundColor          string `json:"topBarBackgroundColor,omitempty"`
-	NavigationBackgroundColor      string `json:"navigationBackgroundColor,omitempty"`
-	ContextBackgroundColor         string `json:"contextBackgroundColor,omitempty"`
-	Errors                         struct {
-		Image string `json:"image,omitempty"`
-	} `json:"errors,omitempty"`
-	ShouldUploadImage bool `json:"shouldUploadImage,omitempty"`
+	HeaderFont                     string `json:"headerFont"`
+	HeaderColor                    string `json:"headerColor"`
+	BodyFont                       string `json:"bodyFont"`
+	BodyColor                      string `json:"bodyColor"`
+	Logo                           string `json:"logo"`
+	Favicon                        string `json:"favicon"`
+	ThemePrimaryColor              string `json:"themePrimaryColor"`
+	ThemeSecondaryColor            string `json:"themeSecondaryColor"`
+	ThemeHighlightColor            string `json:"themeHighlightColor"`
+	ButtonPrimaryBackgroundColor   string `json:"buttonPrimaryBackgroundColor"`
+	ButtonPrimaryTextColor         string `json:"buttonPrimaryTextColor"`
+	ButtonSecondaryBackgroundColor string `json:"buttonSecondaryBackgroundColor"`
+	ButtonSecondaryTextColor       string `json:"buttonSecondaryTextColor"`
+	ButtonDeleteBackgroundColor    string `json:"buttonDeleteBackgroundColor"`
+	ButtonDeleteTextColor          string `json:"buttonDeleteTextColor"`
+	LogoBackgroundColor            string `json:"logoBackgroundColor"`
+	TopBarBackgroundColor          string `json:"topBarBackgroundColor"`
+	NavigationBackgroundColor      string `json:"navigationBackgroundColor"`
+	ContextBackgroundColor         string `json:"contextBackgroundColor"`
+	CardHeaderColor                string `json:"cardHeaderColor"`
+	CardBackground                 string `json:"cardBackground"`
+	CardMenuBackground             string `json:"cardMenuBackground"`
+	CardMenuLinkColor              string `json:"cardMenuLinkColor"`
+	NavigationLinkBackground       string `json:"navigationLinkBackground"`
+	NavigationLinkForeground       string `json:"navigationLinkForeground"`
+	NavigationLinkBackgroundHover  string `json:"navigationLinkBackgroundHover"`
+	NavigationLinkForegroundHover  string `json:"navigationLinkForegroundHover"`
+	NavigationLinkBackgroundActive string `json:"navigationLinkBackgroundActive"`
+	NavigationLinkForegroundActive string `json:"navigationLinkForegroundActive"`
+	TopBarNavigationColor          string `json:"topBarNavigationColor"`
+	TableHeaderBackground          string `json:"tableHeaderBackground"`
+	TableHeaderForeground          string `json:"tableHeaderForeground"`
+	TableBodyBackground            string `json:"tableBodyBackground"`
+	TableBodyForeground            string `json:"tableBodyForeground"`
+	IntercomEnabled                bool   `json:"intercomEnabled"`
+	PendoEnabled                   bool   `json:"pendoEnabled"`
+	DocumentationURL               string `json:"documentationUrl"`
+	ElementsEnabled                bool   `json:"elementsEnabled"`
+	InstanceEnabled                bool   `json:"instanceEnabled"`
+	FormulasEnabled                bool   `json:"formulasEnabled"`
+	VirtualDataEnabled             bool   `json:"virtualDataEnabled"`
+	ReportsEnabled                 bool   `json:"reportsEnabled"`
+	NavigationIconPosition         string `json:"navigationIconPosition"`
+	NavigationIconSize             string `json:"navigationIconSize"`
+	NavigationLabelSize            string `json:"navigationLabelSize"`
+}
+
+// DefaultBranding represents the defaults of CE branding
+var DefaultBranding = BrandingConfig{
+	HeaderFont:                     "museo-sans",
+	HeaderColor:                    "#4d82bf",
+	BodyFont:                       "Open Sans",
+	BodyColor:                      "#333333",
+	ThemePrimaryColor:              "#4d82bf",
+	ThemeSecondaryColor:            "#44c8f5",
+	ThemeHighlightColor:            "#761299",
+	ButtonPrimaryBackgroundColor:   "#4d82bf",
+	ButtonPrimaryTextColor:         "#ffffff",
+	ButtonSecondaryBackgroundColor: "#44c8f5",
+	ButtonSecondaryTextColor:       "#ffffff",
+	ButtonDeleteBackgroundColor:    "#FF4E4E",
+	ButtonDeleteTextColor:          "#ffffff",
+	LogoBackgroundColor:            "#44c8f5",
+	TopBarBackgroundColor:          "#ffffff",
+	NavigationBackgroundColor:      "#172330",
+	ContextBackgroundColor:         "#edf1f2",
+	CardHeaderColor:                "#4d82bf",
+	CardBackground:                 "#ffffff",
+	CardMenuBackground:             "#d1d1d1",
+	CardMenuLinkColor:              "#172330",
+	NavigationLinkBackground:       "#172330",
+	NavigationLinkForeground:       "#ffffff",
+	NavigationLinkBackgroundHover:  "#303a47",
+	NavigationLinkForegroundHover:  "#ffffff",
+	NavigationLinkForegroundActive: "#44c8f5",
+	NavigationLinkBackgroundActive: "#101922",
+	TopBarNavigationColor:          "#172330",
+	TableHeaderBackground:          "#172330",
+	TableBodyBackground:            "#ffffff",
+	TableHeaderForeground:          "#ffffff",
+	TableBodyForeground:            "#333333",
+	NavigationIconSize:             "20px",
+	NavigationLabelSize:            "9px",
 }
 
 // GetBranding returns the Platform's branding
@@ -78,6 +141,10 @@ func SetBranding(base, auth string, branding interface{}, debug bool) ([]byte, i
 	requestbytes, err := json.Marshal(branding)
 	if err != nil {
 		return bodybytes, -1, curlcmd, err
+	}
+	if debug {
+		log.Println("Updating Platform branding ...")
+		log.Println("PUT", url)
 	}
 	bodybytes, status, curlcmd, err = ExecuteWithBody("PUT", url, auth, requestbytes)
 	if debug {
